5e3aa314e61dbc1af2b581764ec0469731ade4f7
Unknown <Hartvigen@gmail.com>
2019-03-07 11:28:00 +0100

    Winforms stopped working

--- FileSorter/MainWindow.xaml.cs
+++ FileSorter/MainWindow.xaml.cs
@@ -23,54 +23,87 @@ namespace FileSorter
     public partial class MainWindow : Window
     {
         private readonly WinForms.FolderBrowserDialog _folderBrowserDialog = new WinForms.FolderBrowserDialog();
+        private readonly string[] _imageExtensions = { ".png", ".jpg", ".gif" };
         private string _fromPath;
-        private string _toPath;
-        private string _imagePath;
-        private ICollectionView _foldersView;
+        private string _toPathUp;
+        private string _toPathDown;
+        private string _toPathRight;
+        private string _toPathLeft;
+        private string _imagePath = @"C:\Users\Dick Kickem\Documents\MEGA\Billeder\Random pics\Wat\6f1.png";
         private string _newFolderName;
 
 
         public MainWindow()
         {
             InitializeComponent();
-            
+            Image1.Source = new BitmapImage(new Uri(imagePath));
+
         }
 
-       public string FromPath
+        public string imagePath
+        {
+            get { return _imagePath; }
+            set { _imagePath = value; }
+
+        }
+
+        public string fromPath
         {
             get { return _fromPath; }
-            set
-            {
-                _fromPath = value;
-            }
+            set { _fromPath = value; }
         }
 
-        public ICollectionView FoldersView
+        public string toPathUp
         {
-            get { return _foldersView; }
-            set
-            {
-                _foldersView = value;
-            }
+            get { return _toPathUp; }
+            set { _toPathUp = value; }
         }
 
 
-        public void OpenToPathOnClick(object sender, RoutedEventArgs e)
+        public void TestImage(object sender, KeyEventArgs e)
         {
+            imagePath = @"C:\Users\Dick Kickem\Documents\MEGA\Billeder\Random pics\Wat\b87.jpg";
+            if (e.Key == Key.E)
+            {
+                Image1.Source = new BitmapImage(new Uri(imagePath));
+            }
+
+            else
+            {
+                MessageBox.Show("pressed something XD");
+            }
+
+        }
+
+        public void TestImageClick(object sender, RoutedEventArgs e)
+        {
+            _imagePath = @"C:\Users\Dick Kickem\Documents\MEGA\Billeder\Random pics\Wat\6f1.png";
+            Image1.Source = new BitmapImage(new Uri(_imagePath));
+        }
+
+
+        public void SendToPath(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                File.Move(_imagePath, _toPathUp);
+            }
+            catch (IOException s)
+            {
+                MessageBox.Show("A problem occured with IO: " + s);
+            }
+        }
+
+        public void OpenToPathUpOnClick(object sender, RoutedEventArgs e)
+        {
+
             var dialog = _folderBrowserDialog;
             var result = dialog.ShowDialog();
 
             if (result != WinForms.DialogResult.OK || !Directory.Exists(_folderBrowserDialog.SelectedPath))
                 return;
-            _toPath = dialog.SelectedPath;
-
-            foreach(var s in Directory.GetDirectories(_toPath))
-            {
-
-            }
+            _toPathUp = dialog.SelectedPath;
         }
 
-
-     
     }
 }
